6b1cc34b3d72b68e190bdff3bd3e226565c36479
XV <44963950+XV@users.noreply.github.com>
2025-06-05 17:08:56 +0800

    创建阳光Prefab

--- PVZ Project/Assets/Scripts/Sunflower.cs
+++ PVZ Project/Assets/Scripts/Sunflower.cs
@@ -1,11 +1,14 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Sunflower : Plant
 {
     public float produceDuration = 5f;
     private float produceTimer = 0f;
-    
     private Animator anim;
+    public GameObject sunPrefab;
+    public float jumpMinDistance = 0.3f;
+    public float jumpMaxDistance = 2f;
 
     private void Awake()
     {
@@ -23,6 +26,11 @@ public class Sunflower : Plant
     }
     public void ProduceSun()
     {
-        print("ProduceSun");
+        GameObject go = GameObject.Instantiate(sunPrefab, transform.position, Quaternion.identity);
+        float distance = Random.Range(jumpMinDistance, jumpMaxDistance);
+        distance = Random.Range(0f, 2f) < 1 ? -distance : distance;
+        Vector3 position = transform.position;
+        position.x += distance;
+        go.GetComponent<Sun>().JumpTo(position);
     }
 }
